de8d17f58cb9f3767a52c66464f5487f2404cc7a
Yana Ustsinovich <yustinovich@icloud.com>
2018-06-04 03:34:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(katz_deli,name)
       katz_deli << (katz_deli + name.split).join
       puts "Welcome, " + "#{name}. " + "You are number " + "1" + " in line."
   elsif katz_deli.count > 0
-    katz_deli << name.split
+    katz_deli << (name.split).join
   else nil
   end
 end
