e46ec1823db8ba33c158f552e28d4a3e7045bc27
Yana Ustsinovich <yustinovich@icloud.com>
2018-06-04 03:08:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def line(deli)
+  katz_deli = []
+  if deli.count > 0
+    deli.each_with_index do |name,index|
+      katz_deli << " #{index+1}. "+"#{name}"
+  end
+      puts "The line is currently:" + katz_deli.join
+  else
+      puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli,name)
+  new_list = []
+  if katz_deli.count == 0
+      new_list << " #{katz_deli[index+1]}. "+"#{name}"
+      puts "W"
+  else nil
+  end
+end
+=begin
+def take_a_number(katz_deli, name)
+katz_deli << name
+  katz_deli.each_with_index do |name,index|
+    puts "Welcome, "+"#{name}."+" You are number "+"#{index+1}"+" in line."
+  end
+end
+
+def now_serving
+
+end
+=end
