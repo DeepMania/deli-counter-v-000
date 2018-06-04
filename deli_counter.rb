9dabbcf0064c8b725fbc49b76e79a2f74a500f75
Yana Ustsinovich <yustinovich@icloud.com>
2018-06-04 03:25:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def take_a_number(katz_deli,name)
   new_list = []
   if katz_deli.count == 0
       new_list << katz_deli + name.split
-      puts "Welcome, " + "#{name}." + "You are number " + new_list[-1] + " in line."
+      puts "Welcome, " + "#{name}." + "You are number " + new_list[-1].join + " in line."
   else nil
   end
 end
